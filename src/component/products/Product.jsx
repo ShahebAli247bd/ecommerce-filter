1a953a014337b1ed831586517234595e8dc553c6
ShahebAli247bd <shahebali247bd@gmail.com>
2025-01-19 15:47:00 +0600

    Data Cached implemented

--- src/component/products/Product.jsx
+++ src/component/products/Product.jsx
@@ -6,18 +6,50 @@ import Card from "./Card";
 import NotFound from "../notfound/NotFound";
 
 const Product = ({ inputTerms }) => {
-    console.log(inputTerms);
+    // console.log(inputTerms);
     String.prototype.toSentenceCase = function () {
-    return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
+        return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
     };
-    
+
     const [fetchData, setFetchData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
 
+    const cache = {
+        data: localStorage.getItem("data")
+            ? JSON.parse(localStorage.getItem("data"))
+            : [],
+        lastFetched: null,
+    };
+
     const getData = async () => {
-        const response = await FetchApi(import.meta.env.VITE_PRODUCT_API);
-        setFetchData(response);
-        setIsLoading(false);
+        try {
+            if (cache.data.length > 0) {
+                console.log("Using cached data");
+                console.log(cache.data);
+                setFetchData(cache.data);
+
+                setIsLoading(false);
+                console.log(cache.data, "i am cache Data from cache block");
+                return;
+            }
+
+            console.log("Fetching data from API");
+
+            const response =
+                cache.data.length <= 0 &&
+                (await FetchApi(import.meta.env.VITE_PRODUCT_API));
+
+            localStorage.setItem("data", JSON.stringify(response));
+
+            // cache.data = response;
+            cache.lastFetched = new Date();
+            setFetchData(response);
+            console.log(cache.data, "i am cache Data from api block");
+        } catch (error) {
+            console.log(error.message);
+        } finally {
+            setIsLoading(false);
+        }
     };
 
     //Call data in refresh
@@ -26,25 +58,26 @@ const Product = ({ inputTerms }) => {
         getData();
     }, []);
 
-//Filtered Data using input Terms 
+    //Filtered Data using input Terms
     const filteredData =
         fetchData?.length >= 0
-            ? fetchData.filter(
-                  (data) =>
-                      data.volumeInfo.title
-                          .toLowerCase()
-                          .includes(inputTerms?.toLowerCase()) ||
-                      inputTerms == "All"? data:"" ||
-                          data.volumeInfo.printType.toLowerCase()
-                        .includes(inputTerms.toLowerCase())
-              
+            ? fetchData.filter((data) =>
+                  data.volumeInfo.title
+                      .toLowerCase()
+                      .includes(inputTerms?.toLowerCase()) ||
+                  inputTerms == "All"
+                      ? data
+                      : "" ||
+                        data.volumeInfo.printType
+                            .toLowerCase()
+                            .includes(inputTerms.toLowerCase())
               )
             : fetchData;
 
     return (
         <div className="product-container">
             <h1>
-                Total:{" "}
+                Total:
                 <span>{filteredData.length ? filteredData.length : 0} </span>
                 {inputTerms ? inputTerms.toSentenceCase() : "Items"}
             </h1>
